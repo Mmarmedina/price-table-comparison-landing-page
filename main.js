356ff713ef9d0c58240ad23f3b8039b4803c14b8
Mmarmedina <maria.martin.84@outlook.es>
2024-06-01 22:30:49 +0200

    Prices table created.The toggle button is not working yet.

---
+++ main.js
@@ -0,0 +1,9 @@
+let priceToggle = document.querySelector('.price-toggler');
+console.log(priceToggle);
+let price = document.getElementById('.price');
+let duration = document.getElementById('.duration');
+
+priceToggle.onclick = () =>{
+    priceToggle.classList.toggle('active');
+    console.log (classList.getElementById('.price-toggle'))
+}
