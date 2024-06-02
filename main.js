6ca4ca9f30f46bd9ff9fa2ea9ac41bc1b5a3e21e
Mmarmedina <maria.martin.84@outlook.es>
2024-06-02 16:48:35 +0200

    The price table toggle is working fine.

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 let priceToggle = document.querySelector('.price-toggler');
-console.log(priceToggle);
-let price = document.getElementById('.price');
-let duration = document.getElementById('.duration');
+let price = document.querySelectorAll('.price');
+let duration = document.querySelectorAll('.duration');
+
+let discount = 1.25;
 
 priceToggle.onclick = () =>{
     priceToggle.classList.toggle('active');
-    console.log (classList.getElementById('.price-toggle'))
+
+    if (priceToggle.classList.contains('active')) {
+        for (let k = 0; k < price.length; k++) {
+            const priceTypeNumber = parseInt(price[k].innerText);
+            console.log (typeof(priceTypeNumber));
+            console.log (priceTypeNumber);
+            price[k].innerText = priceTypeNumber * discount; 
+            price[k].innerText = Math.floor(priceTypeNumber * discount);
+            console.log (price[k]);          
+        }
+    }else {
+        // for (let i = 0; i < duration.length; i++) {
+        //     duration[i].innerText = '/mes';
+            
+        // } 
+        for (let k = 0; k < price.length; k++) {
+            price[k].innerText = price[k].innerText / discount;
+            price[k].innerText = Math.ceil(price[k].innerText);
+        }  
+    }
 }
