aada7bd22a8847127a9927294e21b6ba6bce2a35
Mmarmedina <maria.martin.84@outlook.es>
2024-06-03 10:48:44 +0200

    Mouseover effect in a price comparison table. It's not functioning yet.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let priceToggle = document.querySelector('.price-toggler');
 let price = document.querySelectorAll('.price');
 let duration = document.querySelectorAll('.duration');
+let boxes = document.querySelectorAll('.box');
 
 let discount = 1.25;
 
@@ -10,20 +11,38 @@ priceToggle.onclick = () =>{
     if (priceToggle.classList.contains('active')) {
         for (let k = 0; k < price.length; k++) {
             const priceTypeNumber = parseInt(price[k].innerText);
-            console.log (typeof(priceTypeNumber));
-            console.log (priceTypeNumber);
             price[k].innerText = priceTypeNumber * discount; 
             price[k].innerText = Math.floor(priceTypeNumber * discount);
             console.log (price[k]);          
         }
     }else {
-        // for (let i = 0; i < duration.length; i++) {
-        //     duration[i].innerText = '/mes';
-            
-        // } 
         for (let k = 0; k < price.length; k++) {
             price[k].innerText = price[k].innerText / discount;
             price[k].innerText = Math.ceil(price[k].innerText);
         }  
     }
 }
+
+const handleBoxBackgroundColor = (event) => {
+    const currentBox = event.target
+
+    currentBox.classList.toggle('active');
+
+    if (currentBox.classList.contains('active')){
+        currentBox.classList.add('bg-box');
+        console.log ('hola')
+    }else{
+        currentBox.classList.remove('bg-box');
+        console.log('adios')
+    }
+}
+
+boxes.forEach(box => {
+    box.addEventListener('mouseover',handleBoxBackgroundColor);
+});
+
+
+
+// Cazar el box container
+// Escuchar cuando el ratón se pone encima. 
+// Cuando se ponga encima clase active que se quita cuando se vaya. 
